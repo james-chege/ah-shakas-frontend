0fbb5aa68e1771ae252bbf7c03df05db1b26434a
Ramon3162 <ramonomondi@gmail.com>
2018-11-02 14:53:45 +0300

    Setup store, redux-devetools, and redux-thunk

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from "redux-thunk";
+import rootReducer from "../reducers/index";
+
+const composeEnhancers = composeWithDevTools({
+  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+});
+
+const store = createStore(
+  rootReducer,
+    {}, composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+    // other store enhancers if any
+  )
+);
+
+const storeConfig = () => {
+  return store;
+};
+
+export default storeConfig;

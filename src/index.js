d2caab1b976cf56a911249a7bbd077a98cebb3fe
Ramon3162 <ramonomondi@gmail.com>
2018-11-02 14:52:53 +0300

    Import Provider and set the store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { Provider } from "react-redux";
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import storeConfig from "./store/index";
 
 const history = createHistory();
 
 ReactDOM.render(
-  <BrowserRouter history={history}>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
+  <Provider store = { storeConfig }>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root"),
 );
 
 // If you want your app to work offline and load faster, you can change

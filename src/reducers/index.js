da927d7dc908877a3f4a53784b1ffb157b13b765
james-chege <jymeschege@gmail.com>
2018-11-12 23:50:29 +0300

    feat(reset-password): - Rebase
    - Rebase against develop
    [WIP #160577438]

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import reset from './reset.password.reducer';
 import resetting from './resetting.password.reducer';
-mport signUp from './signup.reducer';
+import signUp from './signup.reducer';
 
 const rootReducer = combineReducers({
   // insert reducers here

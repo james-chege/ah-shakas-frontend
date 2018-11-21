edd0833e9b703e97fd050acc4b14587975d5e10b
Kelvin Lukorito <kelvin.lukorito@andela.com>
2018-11-21 20:39:12 +0300

    feat(tags): Add tags functionality
    	- Add Tags field on Creating Article
    	- Add Tags field on editing articles
    	- Add Tag chips on viewing articles
    	- Add tests for the components
    
    [Finishes #160577448]

--- src/components/Articles/CreateArticleComponent.jsx
+++ src/components/Articles/CreateArticleComponent.jsx
@@ -3,8 +3,14 @@ import { Row, Col } from 'react-materialize';
 import PropTypes from 'prop-types';
 import Navbar from '../Navbar';
 import EditorComponent from './EditorComponent';
+import TagsComponent from './TagsComponent';
+
 
 class CreateArticleComponent extends React.Component {
+  state = {
+    visible: false,
+  };
+
   componentDidUpdate(prevProps) {
     const { history, postArticle, alert } = this.props;
     if (!prevProps.success && postArticle.success) {
@@ -22,7 +28,20 @@ class CreateArticleComponent extends React.Component {
     this.setState(article);
   };
 
+  onTagsChange = (tags) => {
+    this.setState({
+      tags,
+    });
+  };
+
+  handleDropDown = () => {
+    const { visible } = this.state;
+    this.setState({ visible: !visible });
+  };
+
+
   render() {
+    const { visible } = this.state;
     const defaultContent = {
       blocks: [{ text: '', type: 'header-two' }], entityMap: {},
     };
@@ -33,7 +52,12 @@ class CreateArticleComponent extends React.Component {
           <Row>
             <Col s={12}>
               { /* eslint-disable-next-line */ }
-              <p onClick={this.onPublish} className="publish-btn teal-text">Publish </p> 
+              <p onClick={this.handleDropDown} className="publish-btn teal-text">Add Tags </p>
+              <TagsComponent
+                visible={visible}
+                onPublish={this.onPublish}
+                onTagsChange={this.onTagsChange}
+              />
             </Col>
             <Col s={12} className="editor">
               <EditorComponent
